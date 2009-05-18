6b8fb4f313b19eac958d64a742e7445db59a5d13
Trond Marius Ovstetun <trond@ovstetun.no>
2009-05-18 18:00:28 +0200

    Laget service med withdraw og deposit, oppdatert oppgaver

---
+++ test/no/mesan/ooworkshop/service/AccountServiceTest.java
@@ -0,0 +1,49 @@
+package no.mesan.ooworkshop.service;
+
+import static org.junit.Assert.assertEquals;
+import no.mesan.ooworkshop.domain.Account;
+import no.mesan.ooworkshop.domain.CheckingAccount;
+import no.mesan.ooworkshop.domain.Customer;
+import no.mesan.ooworkshop.domain.SavingsAccount;
+import no.mesan.ooworkshop.exception.InnsufficientFundsException;
+
+import org.junit.Test;
+
+public class AccountServiceTest {
+    AccountService service = new AccountService();
+
+    @Test
+    public void depositAddsMoney() throws Exception {
+        Account account = new SavingsAccount(36241604394L, 100.0, new Customer());
+        service.deposit(account, 100.0);
+        
+        assertEquals(200.0, account.getAmount());
+    }
+    
+    @Test
+    public void withdrawRemovesMoney() throws Exception {
+        Account account = new SavingsAccount(36241604394L, 100.0, new Customer());
+        service.withdraw(account, 50.0);
+        
+        assertEquals(50.0, account.getAmount());
+    }
+    
+    @Test(expected=InnsufficientFundsException.class)
+    public void withdrawDoesNotOwerdraw() throws Exception {
+        Account account = new SavingsAccount(36241604394L, 100.0, new Customer());
+        service.withdraw(account, 150.0);
+    }
+    
+    @Test
+    public void withdrawAllowsMoreThanCurrentAmountForCheckingAccount() throws Exception {
+        Account account = new CheckingAccount(36241604394L, 100.0, 70.0, new Customer());
+        service.withdraw(account, 150.0);
+        assertEquals(-50.0, account.getAmount());
+    }
+    
+    @Test(expected=InnsufficientFundsException.class)
+    public void withdrawDoesNotOwerdrawAmountPlusCredit() throws Exception {
+        Account account = new CheckingAccount(36241604394L, 100.0, 70.0, new Customer());
+        service.withdraw(account, 180.0);
+    }
+}
